3bb0aaad0661dc0d78fc9cc1f0e32fac3c1e9dc7
AntropodusKali <sergioparis02@yahoo.com>
2024-10-30 23:52:38 +0100

    Keymaps for git

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,8 @@
 return {
   "tpope/vim-fugitive",
+  cmd={"G","Git"},
+  keys={
+    {"<leader>gps",":Git push",desc="Make a Git push"},
+    {"<leader>gpl",":Git pull",desc="Make a Git pull"},
+  }
 }
