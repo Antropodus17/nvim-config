fcc4a2362f27ad18f68b7e5622041785b71f9e3e
Antropodus17 <a23sergiopn@iessanclemente.net>
2024-11-06 08:53:50 +0100

    Intento 2 de snippets

--- lua/plugins/jssnipet.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "grvcoelho/vim-javascript-snippets",
-    dependencies={
-        "garbas/vim-snipmate",
-        "MarcWeber/vim-addon-mw-utils"
-    },
-    main="config.plugins.jssnipet",
-    config=true,
-}
--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,5 +1,7 @@
 return {
     "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    require("luasnip.loaders.from_vscode").lazy_load(),
     verion="v2.*",
     main="config.plugins.luasnip",
     config=true,
